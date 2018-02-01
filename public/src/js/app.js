ff638b2741e6cbfe22644aec8624ca36d1cddbde
Kevin Kabore <kevin.s.kabore@gmail.com>
2018-02-01 12:57:53 -0800

    Adds logic to control install prompt

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,13 @@
+var defferedPrompt;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function() {
     console.log('Service Worker resgistered');
   });
 }
+
+window.addEventListener('beforeinstallprompt', event => {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  defferedPrompt = event;
+  return false;
+});
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,9 +1,26 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var closeCreatePostModalButton = document.querySelector(
+  '#close-create-post-modal-btn'
+);
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if (defferedPrompt) {
+    defferedPrompt.prompt();
+
+    defferedPrompt.userChoice.then(choiceResult => {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    defferedPrompt = null;
+  }
 }
 
 function closeCreatePostModal() {
